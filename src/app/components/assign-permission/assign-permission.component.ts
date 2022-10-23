16681ea6e8a3e60c5b742d821e43e43a64286995
tarikhamidovic <tarik.hamidovic@gmail.com>
2022-10-23 20:44:55 +0200

    Add permission editing page

--- src/app/components/assign-permission/assign-permission.component.ts
+++ src/app/components/assign-permission/assign-permission.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AppUser } from 'src/app/models/app-user.model';
+import { Permission } from 'src/app/models/permission.enum';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-assign-permission',
@@ -7,9 +13,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssignPermissionComponent implements OnInit {
 
-  constructor() { }
+  assignPermissionsForm = this.formBuilder.group({
+    description: false,
+    code: false
+  });
+  existingAppUser: AppUser = {
+    id: 0,
+    firstName: '',
+    lastName: '',
+    password: '',
+    email: '',
+    status: '',
+    permissions: []
+  };
+  existingPermissions: Permission[] = [];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private toast: ToastrService
+  ) { }
 
   ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const userId = Number(routeParams.get('userId'));
+
+    this.userService.getUserById(userId).subscribe((appUser) => {
+      this.existingAppUser = appUser;
+      this.existingPermissions = appUser.permissions;
+      this.assignPermissionsForm.setValue({
+        description: this.hasPermission(Permission.Description),
+        code: this.hasPermission(Permission.Code)
+      });
+    });
   }
 
+  onSubmit() {
+    const codePermission = this.assignPermissionsForm.value.code;
+    const descriptionPermission = this.assignPermissionsForm.value.description;
+
+    const newPermissions = [];
+    if (codePermission) newPermissions.push(Permission.Code);
+    if (descriptionPermission) newPermissions.push(Permission.Description);
+
+    this.existingAppUser.permissions = newPermissions;
+    this.userService.updateUser(this.existingAppUser).subscribe(
+      (appUser) => {
+        this.existingAppUser = appUser;
+        this.existingPermissions = appUser.permissions;
+        this.toast.success('Successfully updated permissions for user', 'Success');
+      },
+      () => this.toast.error('Error updating permissions for user', 'Error')
+    );
+  }
+
+  private hasPermission(permission: Permission): boolean {
+    return this.existingPermissions.includes(permission);
+  }
 }
--- src/app/models/permission.enum.ts
+++ src/app/models/permission.enum.ts
@@ -1,4 +1,4 @@
 export enum Permission {
-    Code,
-    Description
+    Code = 'Code',
+    Description = 'Description'
 }
